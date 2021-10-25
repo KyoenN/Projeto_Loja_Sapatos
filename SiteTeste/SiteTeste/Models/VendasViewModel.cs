72d9583aa67db0fedbb8fbc187cb73c8bbf6d984
daniellafc <daniellaferreiracoutinho@gmail.com>
2021-10-24 21:53:43 -0300

    Criação da tela Cadastro de modelos e alteração na home

---
+++ SiteTeste/SiteTeste/Models/VendasViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteTeste.Models
+{
+    public class VendasViewModel
+    {
+        public List<string> codRef { get; set; }
+        public List<int> id_modelo { get; set; }
+        public List<string> nome_modelo { get; set; }
+        public List<string> nome_fornecedor { get; set; }
+        public List<string> cor_modelo { get; set; }
+        public List<int> tamanho_modelo { get; set; }
+        public List<float> valorItem { get; set; }
+        public List<int> quantidade { get; set; }
+        public int id_cliente { get; set; }
+        public string nome_cliente { get; set; }
+        public DateTime dtVenda { get; set; }
+        public float total { get; set; }
+
+    }
+}
