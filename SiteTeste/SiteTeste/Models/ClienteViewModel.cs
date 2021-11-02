80a82845a98358c6a411a80afef41f951f7bef35
lucianogusmao <luciano_po@hotmail.com>
2021-11-01 21:37:57 -0300

    Viewcliente

---
+++ SiteTeste/SiteTeste/Models/ClienteViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteTeste.Models
+{
+    public class ClienteViewModel
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string CPF { get; set; }
+        public string endereço { get; set; }
+        public string sexo { get; set; }
+    }
+}
